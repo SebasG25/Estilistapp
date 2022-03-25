f6728dd6aac31b736c9381863ea1cce26979fdb8
Sebastian Guzman <bitas200225@gmail.com>
2022-03-24 19:01:26 -0500

    Prueba Deploy

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -7,7 +7,7 @@ import { Slider } from '../components/Slider/Slider'
 export const LandingPage = () => {
   return (
     <div>
-      <Hero imageSrc={image_01} title={'Landing Page'}/>
+      <Hero imageSrc={image_01} title={'Home'}/>
       <Slider imageSrc={image_02} title={'Basic text'} subtitle={'Basic subtitle'} />
       <Slider imageSrc={image_03} title={'Basic text'} subtitle={'Basic subtitle'} flipped={true} />
     </div>
